66ce1a9ceea194b0edf2f2481228a33884ad6483
ulambayar0511 <ulmaaulambayar4@gmail.com>
2023-03-12 06:33:21 -0400

    add lab5

---
+++ lab5/bye.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <netinet/in.h>
+#define MAX_CLIENTS 10
+
+void *handle_client(void *arg);
+
+int main(int argc, char *argv[]) {
+    int server_fd, client_fd;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    pthread_t threads[MAX_CLIENTS];
+
+    // Create socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket options
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("Setsockopt error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind socket
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for connections
+    if (listen(server_fd, MAX_CLIENTS) < 0) {
+        perror("Listen error");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is listening on port 8080\n");
+
+    // Accept client connections and create a thread for each
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+        // This is the child process
+            printf("Child process created: PID = %d, PPID = %d\n", getpid(), getppid());
+        }
+        if ((client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+            perror("Accept error");
+            exit(EXIT_FAILURE);
+        }
+        printf("Client %d connected\n", i+1);
+        pthread_create(&threads[i], NULL, handle_client, (void *)&client_fd);
+    }
+
+    // Join threads
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return 0;
+}
+
+void *handle_client(void *arg) {
+    int client_fd = *(int *)arg;
+    char buffer[1024] = {0};
+    int valread;
+    char *message = "Hello from server\n";
+    if (send(client_fd, message, strlen(message), 0) == -1) {
+        perror("Send failed");
+        exit(1);
+    }
+    // Read from client
+    valread = read(client_fd, buffer, 1024);
+    char bye[3] = "bye";
+    printf("Received: %s\n", buffer);
+    if (strncmp(buffer,bye,3) == 0) {
+        printf("Client disconnected\n");
+        close(client_fd);
+        pthread_exit(NULL);
+    } else {
+        printf("wtf");
+    }
+    char message[1024];
+    scanf("%s",&message);
+
+ 
+}
---
+++ lab5/bye_client.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]) {
+    // Check for correct number of arguments
+    if (argc != 3) {
+        printf("Usage: %s <Server IP> <Server Port>\n", argv[0]);
+        exit(1);
+    }
+
+    // Create a socket
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("Socket creation failed");
+        exit(1);
+    }
+
+    // Set up the server address
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    server_addr.sin_port = htons(atoi(argv[2]));
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        exit(1);
+    }
+
+    // Receive a message from the server
+    while (1) {
+        char buffer[1024];
+        int num_bytes = recv(sock, buffer, sizeof(buffer), 0);
+        if (num_bytes == -1) {
+            perror("Receive failed");
+            exit(1);
+        }
+
+        // Print the message received from the server
+        buffer[num_bytes] = '\0';
+        printf("Received message from server: %s\n", buffer);
+        printf("Send message to server:");
+        char message[1024];
+        scanf("%s",&message);
+        if (strncmp(message,"bye",3) == 0) {
+            close(sock);
+            exit(1);
+        }
+        if (send(sock, message, strlen(message), 0) == -1) {
+            perror("Send failed");
+            exit(1);
+        }
+        // Close the socket
+        
+    }
+    close(sock);
+    return 0;
+}
